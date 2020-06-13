f5748c4c9ddc168b2a23c8bb7f25bae25fa75b87
MarB101 <martinbone16@gmail.com>
2020-06-13 14:40:50 +0100

    Updates

---
+++ precache-manifest.499f31ae07918ca294a6e653a033325d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dc99e05a5ffaec41338f87ee90227f3",
+    "url": "/demoSite/index.html"
+  },
+  {
+    "revision": "39f0ca9601ce906227d3",
+    "url": "/demoSite/static/css/2.9c6960d2.chunk.css"
+  },
+  {
+    "revision": "ee4e5ef8b30f7d554d20",
+    "url": "/demoSite/static/css/main.aaef4f27.chunk.css"
+  },
+  {
+    "revision": "39f0ca9601ce906227d3",
+    "url": "/demoSite/static/js/2.102ab804.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/demoSite/static/js/2.102ab804.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee4e5ef8b30f7d554d20",
+    "url": "/demoSite/static/js/main.56687a32.chunk.js"
+  },
+  {
+    "revision": "ae82db3051b6c12b6f24",
+    "url": "/demoSite/static/js/runtime-main.5666ed09.js"
+  }
+]);
